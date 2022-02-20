80553b470e4ca2b471778638f47c903a65b705a9
Shania Balkhi <balkhi.shania@gmail.com>
2022-02-20 12:15:58 +0530

    Create FindTheUniqueElement.java

---
+++ 6. Time Complexity/FindTheUniqueElement.java
@@ -0,0 +1,9 @@
+class Solution {
+	public static int findUnique(int[] arr) {
+        int unique = 0;
+		for(int i = 0; i < arr.length; ++i){
+            unique ^= arr[i];
+        }
+        return unique;
+	}
+}
