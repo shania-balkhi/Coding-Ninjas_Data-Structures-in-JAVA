4c984febc0857208658bc20b78dc2e753258e7e8
Shania Balkhi <balkhi.shania@gmail.com>
2022-02-21 19:15:26 +0530

    Create ArrayIntersection.java

---
+++ 6. Time Complexity/ArrayIntersection.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class Solution {
+    public static void intersection(int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        mergeSortTypeApproach(arr1, arr2);
+    }
+
+    private static void mergeSortTypeApproach(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+            }
+
+            else if (arr1[i] < arr2[j])
+                i++;
+
+            else
+                j++;
+        }
+        
+    }
+}
