a42e0f5abdd3624e7fa2337864a58ea84c140d8b
Shania Balkhi <balkhi.shania@gmail.com>
2022-02-22 16:19:49 +0530

    Update PairSumInArray.java

--- 6. Time Complexity/PairSumInArray.java
+++ 6. Time Complexity/PairSumInArray.java
@@ -1,46 +1,44 @@
 import java.util.Arrays;
-
-class Solution {	
-
+public class Solution {	
 	public static int pairSum(int[] arr, int num) {
-		Arrays.sort(arr);
-        int i = 0, j = arr.length - 1;
-        int count = 0, count_i = 1, count_j = 1;
-        while(i < j){
-            if(arr[i] + arr[j] == num){
-                if(arr[i]==arr[j]){
-                    count += ((j-i)*(j+1-i))/2;
-                    return count;
+        Arrays.sort(arr);
+		int numOfPairs = 0;
+        int si = 0, ei = arr.length - 1;
+        while(si < ei){ 
+            if (arr[si] + arr[ei] < num){
+                ++si;
+            }else if(arr[si] + arr[ei] > num){
+                --ei;
+            }else{
+                /* for edge cases like 2 2 2 2 2 */
+                if (arr[si] == arr[ei]){
+                    int totalNumOfElementsFromStartToEnd = (ei -si) + 1;
+                    numOfPairs += (totalNumOfElementsFromStartToEnd * (totalNumOfElementsFromStartToEnd - 1))/2;
+                    return numOfPairs;
+                } 
+                
+                /* for edge cases like 1 2 3 3 5 6 9 9 9 10*/
+                int tempSI = si + 1; //tempSI = temporaryStartIndex
+                int tempEI = ei - 1; //tempEI = temporaryEndIndex
+                
+                while(tempSI <= tempEI && arr[tempSI] == arr[si]){
+                    ++tempSI;
                 }
-                else{
-                    for(int k = i + 1; k < j; ++k){
-                        if(arr[i] == arr[k]){
-                            count_i++;
-                            i=k;
-                        }
-                    }
-                    for(int k = j-1; k > i; k--){
-                        if(arr[j] == arr[k]){
-                            count_j++;
-                            j=k;
-                        }
-                    }
-                    count += count_i * count_j;
-                    count_i =1;
-                    count_j=1;
+                
+                while(tempEI >= tempSI && arr[tempEI] == arr[ei]){
+                    --tempEI;
                 }
-                i++;
-                j--;
+                
+                int totalNumOfDuplicateElementsFromStart = (tempSI - si);     
+                int totalNumOfDuplicateElementsFromEnd = (ei - tempEI);      
+                
+                numOfPairs += (totalNumOfDuplicateElementsFromStart) * (totalNumOfDuplicateElementsFromEnd);
+                
+                si = tempSI;
+                ei = tempEI;
+                
             }
-
-            else if(arr[i] + arr[j] > num){
-                --j;
-            }
-
-            else{
-                ++i;
-            }
-        }
-        return count;
+		}
+        return numOfPairs;
 	}
 }
