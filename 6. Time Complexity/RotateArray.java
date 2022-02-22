1d47fe958089bdaa2a4903bdd211cb57ef8a9d38
Shania Balkhi <balkhi.shania@gmail.com>
2022-02-22 18:01:55 +0530

    Update RotateArray.java

--- 6. Time Complexity/RotateArray.java
+++ 6. Time Complexity/RotateArray.java
@@ -1,3 +1,33 @@
+//1st approach (optimized)
+//Space Complexity : O(1)
+//Time Complexity : O(N)
+public class Solution {  
+    public static void rotate(int[] arr, int d) {
+    	reverse(arr, 0, d - 1);
+        reverse(arr, d, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+    }
+    
+    private static void reverse(int[] arr, int si, int ei){  //si = startIndex, ei = endIndex
+        while(si < ei){
+            int temp = arr[si];
+            arr[si] = arr[ei];
+            arr[ei] = temp;
+            
+            ++si;
+            --ei;
+        }
+    }
+}
+
+
+/****************************************************************************************************************************************************************************/
+/****************************************************************************************************************************************************************************/
+
+
+//2nd approach 
+//Space Complexity : O(N)
+//Time Complexity : O(N)
 public class Solution {
     public static void rotate(int[] arr, int d) {
         int[] temp = new int[arr.length];
