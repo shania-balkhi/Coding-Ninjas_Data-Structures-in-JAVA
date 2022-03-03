3d0459bca806df5c6e6771c4bd67877d72fd8085
Shania Balkhi <balkhi.shania@gmail.com>
2022-03-03 14:21:47 +0530

    Create PrintReverseLinkedList.java

---
+++ 10. Linked List - I/PrintReverseLinkedList.java
@@ -0,0 +1,9 @@
+public class Solution {
+	public static void printReverse(LinkedListNode<Integer> root) {
+		if(root == null){
+            return;
+        }
+        printReverse(root.next);
+        System.out.print(root.data + " ");
+	}
+}
