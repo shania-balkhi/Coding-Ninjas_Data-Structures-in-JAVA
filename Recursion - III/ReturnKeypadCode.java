0dcf6fc36571d99611efc4ee1bfd843542d3ec95
Shania Balkhi <balkhi.shania@gmail.com>
2022-03-16 20:03:24 +0530

    Update ReturnKeypadCode.java

--- Recursion - III/ReturnKeypadCode.java
+++ Recursion - III/ReturnKeypadCode.java
@@ -1,3 +1,4 @@
+//code
 public class solution {
     private static String getOptions(int digit){
          if(digit == 2){
@@ -36,15 +37,19 @@ public class solution {
         }
         
         String[] smallOutput = keypad(n/10);
+        //it's easier to pass the first (n-1) digits since for that /10 would work all fine. but if we decide to pass the last (n-1) digits 
+        //then we'll have to figure out the the multiple of 10 by which we will divide the input digit
         
-        int lastDigit = n % 10;
-        String lastDigitOptions = getOptions(lastDigit);
-        String[] output = new String[smallOutput.length * lastDigitOptions.length()];
+        int lastDigit = n % 10; //to find the last digit
+        String lastDigitOptions = getOptions(lastDigit); //to know how many alphabets corresponding to the last digit exists in the keypad. for eg, 4 has 3(d,e,f) & 7 has 4(p,q,r,s)
+        String[] output = new String[smallOutput.length * lastDigitOptions.length()]; //delcare the output array of size as mentioned 
         
-        int k = 0;
-        for(int i = 0; i < smallOutput.length; ++i){
+        int k = 0; //this is the index of the output array where each outputElement needs to be stored
+        
+        //just append the lastDigit's alphabets to the smallOutputArrayElements to obtain the final output array
+        for(int i = 0; i < smallOutput.length; ++i){  
             for(int j = 0; j < lastDigitOptions.length(); ++j){
-                output[k] = smallOutput[i] + lastDigitOptions.charAt(j);
+                output[k] = smallOutput[i] + lastDigitOptions.charAt(j); //since we chose to work on the first (n-1) digits for convenience, we gotta append the laslDigit's corresponding alphabets towards the end. hence it's concatenated second to the smallOutputArray 
                 ++k;
             }
         }
