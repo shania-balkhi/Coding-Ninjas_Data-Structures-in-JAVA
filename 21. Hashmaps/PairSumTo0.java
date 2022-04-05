4f3dbe28923e143d6c0053fda828cdfae76cf631
Shania Balkhi <balkhi.shania@gmail.com>
2022-04-05 12:32:11 +0530

    Update PairSumTo0.java

--- 21. Hashmaps/PairSumTo0.java
+++ 21. Hashmaps/PairSumTo0.java
@@ -23,9 +23,9 @@ Sample Output 1:
 
 
 
-/*algo-
-1. put the input array elements into the hashmap (line 52 to 62)
-2. initialise a count variable(line 64) and iterate over the range of arraySize. (line 65)
+/*algo (approach 1 - using 2 loops) -
+1. put the input array elements into the hashmap (line 53 to 63)
+2. initialise a count variable(line 64) and iterate over the range of arraySize. (line 66)
 3. for 'count' to compute, the frequency of the ith arrayElement should be > 0 in the HashMap. so that's always the 
    first thing we check in any of the conditional statements.
 4. if the ith element is 0 : since 0 does not have any +ve or -ve counterparts, we simply count the pairCount of 0 using the 
@@ -44,40 +44,109 @@ count should always be succeeded by the operator "+=" and not just "=", in order
 
 
 
+// approach 1 (using 2 loops)
+//code - 1
+// import java.util.*;
+
+// class Solution {
+// 	public static int PairSum(int[] input, int size) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+// 		for(int i = 0; i < size; ++i){
+//             if(map.containsKey(input[i])){
+//                 int oldFrequency = map.get(input[i]);
+//                 int updatedFrequency = oldFrequency + 1;
+//                 map.put(input[i], updatedFrequency);
+//             }else{
+//                 map.put(input[i], 1);
+//             }
+//         }
+
+//         int count = 0;
+//         for(int i = 0; i < size; ++i){
+//             int freq_ith = map.get(input[i]);
+//             if(freq_ith > 0 && input[i] == 0){
+//                 count += (freq_ith * (freq_ith - 1)) / 2;
+//                 map.put(input[i], 0);
+//             }
+            
+//             else if(map.containsKey(-input[i])){
+//                 int freq_minus_ith = map.get(-input[i]);
+//                 if(freq_ith > 0 && freq_minus_ith > 0){
+//                 	count += freq_ith * freq_minus_ith;
+//                 	map.put(input[i], 0);
+//                 	map.put(-input[i], 0);
+//             	}
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+algo 2 (for approach 2 - optimized) - using single for loop
+1. in this approach, we count the number of Pairs while we go traversing (once) the arrayElements.
+2. initialise two variables 'count' and 'freq_of_minus_ith_element' with 0
+3. iterate over the range of arraySize
+4. check if the map contains the ith_element :
+   4.1 if it does not contain then, check whether the map contains minus_ith_element
+        4.1.1 if it does then, count will be 'count += freq_minus_ith_element'
+        4.1.2 then, insert the ith_element in the map i.e., 'map.put(input[i], 1)'
+   4.2 if the map contains the ith element, then repeat steps 4.1 and 4.1.1 (not 4.1.2)
+   		4.2.1 evaluate the updatedFrequency_of_ith_element and put it in the map.
+5. return count
+6.note that this approach also takes care of 'all zeros case' as well!
+
+
+note -
+in line 39, note that the line will be :
+			count += freq_of_minus_ith_element 
+            and not,
+            count += oldFrequency_of_ith_element * freq_of_minus_ith_element (this will give WA) 
+*/
+
+
+
+//approach 2 - using single for loop - optimized
 //code - 2
 import java.util.*;
 
-class Solution {
+public class Solution {
 	public static int PairSum(int[] input, int size) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-		for(int i = 0; i < size; ++i){
+		HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int freq_of_minus_ith_element = 0;
+        for(int i = 0; i < size; ++i){
             if(map.containsKey(input[i])){
-                int oldFrequency = map.get(input[i]);
-                int updatedFrequency = oldFrequency + 1;
-                map.put(input[i], updatedFrequency);
+                if(map.containsKey(-input[i])){
+                    freq_of_minus_ith_element = map.get(-input[i]);
+                    count += freq_of_minus_ith_element;
+                }
+                int oldFrequency_of_ith_element = map.get(input[i]);
+                int updatedFrequency_of_ith_element = oldFrequency_of_ith_element + 1;
+                map.put(input[i], updatedFrequency_of_ith_element);
             }else{
+                if(map.containsKey(-input[i])){
+                    freq_of_minus_ith_element = map.get(-input[i]);
+                    count += freq_of_minus_ith_element;
+                }
                 map.put(input[i], 1);
             }
         }
-
-        int count = 0;
-        for(int i = 0; i < size; ++i){
-            int freq_ith = map.get(input[i]);
-            if(freq_ith > 0 && input[i] == 0){
-                count += (freq_ith * (freq_ith - 1)) / 2;
-                map.put(input[i], 0);
-            }
-            
-            else if(map.containsKey(-input[i])){
-                int freq_minus_ith = map.get(-input[i]);
-                if(freq_ith > 0 && freq_minus_ith > 0){
-                	count += freq_ith * freq_minus_ith;
-                	map.put(input[i], 0);
-                	map.put(-input[i], 0);
-            	}
-            }
-        }
         return count;
-    }
+	}
 }
