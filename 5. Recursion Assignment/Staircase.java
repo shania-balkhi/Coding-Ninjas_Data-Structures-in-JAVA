62995c1fd2c9754ed3a62edaf21a0ed1afb1948f
Shania Balkhi <balkhi.shania@gmail.com>
2022-05-15 16:50:11 +0530

    Update Staircase.java

--- 5. Recursion Assignment/Staircase.java
+++ 5. Recursion Assignment/Staircase.java
@@ -1,6 +1,8 @@
 /*Staircase
 
-A child is running up a staircase with N steps, and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count how many possible ways the child can run up to the stairs. You need to return number of possible ways W.
+A child is running up a staircase with N steps, and can hop either 1 step, 2 steps or 3 steps at a time.
+Implement a method to count how many possible ways the child can run up to the stairs. 
+You need to return number of possible ways W.
 Input format :
 Integer N
 Output Format :
