8ef83625581a7a60545a428c3a1cae425e24d8f8
Shania Balkhi <balkhi.shania@gmail.com>
2022-06-16 01:13:17 +0530

    Update LCS-Problem.java

--- 28. Dynamic Programming - II/LCS-Problem.java
+++ 28. Dynamic Programming - II/LCS-Problem.java
@@ -178,3 +178,30 @@ public class Solution {
 	       
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+//3. Iterative DP Solution
+public class Solution {
+
+	public static int lcs(String s, String t) {
+        
+		int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for(int i = 0; i < dp.length; ++i){
+            for(int j = 0; j < dp[i].length; ++j){
+                dp[i][j] = 0;
+            }
+        }
+        
+        for(int i = dp.length - 2; i >= 0; --i){
+            for(int j = dp[i].length - 2; j >= 0; --j){
+                if(s.charAt(i) == t.charAt(j)){
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
+
+}
